0db778a3491d3c6ddc78fd0b8501eea1d6d9b022
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-01-04 00:44:49 -0300

    new commit

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,20 @@
+import { TouchableOpacity } from "react-native";
+import { Container, FilterStyleProps, Title } from "./styles";
+
+type Props = TouchableOpacity & FilterStyleProps & {
+    title: string;
+}
+
+export function Filter({ title, isActive = false, ...rest }: Props) {
+    return (
+        <Container
+            isActive={isActive}
+            {...rest}
+        >
+
+            <Title>
+                {title}
+            </Title>
+        </Container>
+    );
+}
